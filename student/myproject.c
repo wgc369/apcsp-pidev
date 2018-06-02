7ba8fd464ca6ac91caf2a445bf381df013490d5e
wgc369 <guanchen2002@gmail.com>
2018-06-01 17:04:51 -0700

    5/31/2018

---
+++ student/myproject.c
@@ -0,0 +1,18 @@
+// hellomake.c
+//
+// example C program to demo multiple source files and Makefile
+//
+
+#include <stdio.h>
+#include <string.h>
+
+// include a header file
+#include "student.h"
+
+int main()
+{
+  // call function foo in hellofunc
+  student();
+  
+//  printf("Hello World! %d\n", s);
+}
---
+++ student/student.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include "student.h"
+
+struct Student {
+  char fn[50];
+  char ln[50];
+  char a[10];
+  char si[50];
+};
+
+void printStudent(struct Student* student)
+{
+  printf("the student is -\n");
+  printf("  first name:%s\n", student->fn);
+  printf("  last name:%s\n", student->ln);
+  printf("  age:%s\n", student->a);
+  printf("  student id:%s\n", student->si);
+}
+
+int student()
+{
+  struct Student arr[3];
+
+  for (int i = 0; i < 3; i++)
+  {
+    printf("Enter Your First Name:\n");
+    scanf("%s", arr[i].fn);
+    printf("Enter Your Last Name:\n");
+    scanf("%s", arr[i].ln);
+    printf("Enter Your Age:\n");
+    scanf("%s", arr[i].a);
+    printf("Enter Your Student ID:\n");
+    scanf("%s", arr[i].si);
+  }
+  for (int i = 0; i < 3; i++)
+  {
+    printStudent(&arr[i]);
+  }
+  return 0;
+}
